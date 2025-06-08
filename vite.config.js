dac1c705bcdd53ee8396b2011c75f1490662e08d
honddk <honddk99@gmail.com>
2025-06-08 17:39:34 +0500

    new base in config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  base: '/diploma-presentation/'
+  base: 'https://honddk.github.io/diploma-presentation/'
 })
